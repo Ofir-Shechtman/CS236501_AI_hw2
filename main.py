fd3ec7a0169940790a3f3daa399d1638fe26eebb
Tomershmil <tomershmil@gmail.com>
2020-12-21 09:14:43 +0200

    starting second day

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ if __name__ == "__main__":
     parser.add_argument('-player1', default='LivePlayer', type=str,
                         help='The type of the first player.',
                         choices=players_options)
-    parser.add_argument('-player2', default='SimplePlayer',  type=str,
+    parser.add_argument('-player2', default='MinimaxPlayer',  type=str,
                         help='The type of the second player.',
                         choices=players_options)
     
